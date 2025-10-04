aa6b57b17c02fb67725f807eba02dafeebaf0407
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 11:09:11 -0700

    Refactored package to implement automatic installation & configuration in TokenRing apps

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
-import type { TokenRingPackage } from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
 
 import * as chatCommands from "./chatCommands.ts";
-import packageJSON from "./package.json" with { type: "json" };
+import packageJSON from "./package.json" with {type: "json"};
 import * as tools from "./tools.ts";
+import WorkQueueService from "./WorkQueueService.js";
 
 export const packageInfo: TokenRingPackage = {
 	name: packageJSON.name,
 	version: packageJSON.version,
 	description: packageJSON.description,
-	chatCommands,
-	tools,
+  install(agentTeam: AgentTeam) {
+    agentTeam.addTools(packageInfo, tools)
+    agentTeam.addChatCommands(chatCommands);
+    agentTeam.addServices(new WorkQueueService());
+  },
 };
 
 export { default as WorkQueueService } from "./WorkQueueService.ts";
