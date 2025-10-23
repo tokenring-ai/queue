130b9c5fe3e2285884c16de65480352a82b87bb8
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-23 14:24:48 -0700

    Converted the packageInfo export to a default export

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
-import { AgentTeam, TokenRingPackage } from "@tokenring-ai/agent";
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
 
 import * as chatCommands from "./chatCommands.ts";
-import packageJSON from "./package.json" with { type: "json" };
+import packageJSON from "./package.json" with {type: "json"};
 import * as tools from "./tools.ts";
 import WorkQueueService from "./WorkQueueService.js";
 
-export const packageInfo: TokenRingPackage = {
-	name: packageJSON.name,
-	version: packageJSON.version,
-	description: packageJSON.description,
-	install(agentTeam: AgentTeam) {
-		agentTeam.addTools(packageInfo, tools);
-		agentTeam.addChatCommands(chatCommands);
-		agentTeam.addServices(new WorkQueueService());
-	},
-};
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(agentTeam: AgentTeam) {
+    agentTeam.addTools(packageJSON.name, tools);
+    agentTeam.addChatCommands(chatCommands);
+    agentTeam.addServices(new WorkQueueService());
+  },
+} as TokenRingPackage;
 
-export { default as WorkQueueService } from "./WorkQueueService.ts";
+export {default as WorkQueueService} from "./WorkQueueService.ts";
